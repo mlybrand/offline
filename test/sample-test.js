ab67f5cd0c7236884ec323dc7e9ecda25f4c9e78
Mark Lybrand <mlybrand@gmail.com>
2015-05-16 23:51:18 -0700

    added zombie and express and started a simple server

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,7 +1,23 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    Browser = require('zombie'),
+    app = require('../lib/app');
 
-describe("foo", function() {
-    it('will pass', function() {
-        expect('bar').to.equal("bar");
+describe("Server", function() {
+    var server;
+
+    before(function() {
+        server = app.listen(3001);
+    });
+
+    after(function() {
+        server.close();
+    });
+
+    it('should respond when called', function(done) {
+        var browser = new Browser();
+        browser.visit("http://localhost:3001", function() {
+            expect(browser.text("#page-heading")).to.equal("Hello World");
+            done();
+        });
     });
 });
