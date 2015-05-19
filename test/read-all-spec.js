8a1ebf517844be657c28619399ef62e0acfdbab3
Mark Lybrand <mlybrand@gmail.com>
2015-05-18 23:29:56 -0700

    wireframe sort of

--- test/read-all-spec.js
+++ test/read-all-spec.js
@@ -2,11 +2,7 @@ describe.only("Default View (Read All)", function() {
     it("should return a list of items");
     describe("Each item", function() {
         it("should have a name");
-        it("should have a location");
-        describe("Each location", function() {
-            it("should have a latitude and longitude");
-        });
-        it("should have an id number");
+        it("should have an id number that is link");
         it("should have a rating");
     });
 });
