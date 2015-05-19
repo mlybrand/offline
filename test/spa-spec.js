d40ed6783f849dab68340bde3ab7a17298309695
Mark Lybrand <mlybrand@gmail.com>
2015-05-19 00:12:33 -0700

    added first failing test

--- test/spa-spec.js
+++ test/spa-spec.js
@@ -1,5 +1,28 @@
-describe("SPA", function() {
-    it("should have a header section with the site title");
+var expect = require('chai').expect,
+    Browser = require('zombie'),
+    app = require('../lib/app');
+
+describe.only("SPA", function() {
+    var server,
+        browser = new Browser();
+
+    before(function() {
+        server = app.listen(3001);
+    });
+
+    after(function() {
+        server.close();
+    });
+
+    beforeEach(function() {
+        return browser.visit("http://localhost:3001");
+    });
+
+    it("should have a header section with the site title", function() {
+        expect(browser.queryAll('#header')).to.have.length(1);
+        expect(browser.text('#header .logo')).to.equal('Offline Inventory');
+    });
+
     it("should have an area for the list of available inventory items");
     it("should have an area for the selected inventory item");
     it("should have a footer with copyright information");
