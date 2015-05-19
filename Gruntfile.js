7754502e5146313a2d2041cba4b37406f89d9513
Mark Lybrand <mlybrand@gmail.com>
2015-05-19 11:46:41 -0700

    added bower and used it to add bootstrap and jquery. also added tasks to grunt to run bower and to copy files to public folder for static delivery via express

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,25 @@ var grunt = require("grunt");
 
 grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    bower: {
+        install: {
+            options: {
+                copy: false
+            }
+        }
+    },
+    copy: {
+        main: {
+            files: [
+                { cwd: 'bower_components/bootstrap/dist/', src: '**/*', dest: 'public/vendor', expand: true },
+                { cwd: 'bower_components/jquery/dist/', src: '*', dest: 'public/vendor/js', expand: true }
+            ]
+        }
+    },
     jshint: {
         all: ['**/*.js'],
         options: {
-            ignores: ['node_modules/**/*.js']
+            ignores: ['node_modules/**/*.js', 'bower_components/**/*.js', 'public/vendor/**/*.js' ]
         }
     },
     mochaTest: {
@@ -16,8 +31,10 @@ grunt.initConfig({
 });
 
 grunt.loadNpmTasks('grunt-npm-install');
+grunt.loadNpmTasks('grunt-bower-task');
+grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-mocha-test');
 
-grunt.registerTask('default', ['npm-install', 'test']);
+grunt.registerTask('default', ['npm-install', 'bower:install', 'copy', 'test']);
 grunt.registerTask('test', ['jshint', 'mochaTest']);
