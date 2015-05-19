7dd4df4dd0624b076b05e224871bfbaacb0dd8cb
Mark Lybrand <mlybrand@gmail.com>
2015-05-18 23:55:16 -0700

    redefined spec to focus on overall page layout

--- test/read-all-spec.js
+++
@@ -1,8 +0,0 @@
-describe.only("Default View (Read All)", function() {
-    it("should return a list of items");
-    describe("Each item", function() {
-        it("should have a name");
-        it("should have an id number that is link");
-        it("should have a rating");
-    });
-});
---
+++ test/spa-spec.js
@@ -0,0 +1,15 @@
+describe("SPA", function() {
+    it("should have a header section with the site title");
+    it("should have an area for the list of available inventory items");
+    it("should have an area for the selected inventory item");
+    it("should have a footer with copyright information");
+});
+
+//describe.only("Default View (Read All)", function() {
+//    it("should return a list of items");
+//    describe("Each item", function() {
+//        it("should have a name");
+//        it("should have an id number that is link");
+//        it("should have a rating");
+//    });
+//});
