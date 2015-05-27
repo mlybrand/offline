66a6f58ab49d13a7e5403468d86b389623633f3d
Mark Lybrand <mlybrand@gmail.com>
2015-05-27 13:57:17 -0700

    implementd create functionality

--- public/js/script.js
+++ public/js/script.js
@@ -34,14 +34,35 @@ $(function () {
         var makeDirty = function() {
             activeItem.dirty(true);
         };
+        function generateNextId() {
+            var maxId = (_.max(inventory(), function(item) {
+                return item.id();
+            })).id(),
+                nextId = "0000000" +  (parseInt(maxId, 10) + 1);
+            return nextId.substr(nextId.length - 7);
+        }
+        function addEntry(item) {
+            item.id(generateNextId());
+            inventory.push({
+                id: ko.observable(item.id()),
+                name: ko.observable(item.name()),
+                rating: ko.observable(item.rating()),
+                dirty: ko.observable(false)
+            });
+            resetForm();
+        }
         var updateEntry = function() {
+            if (activeItem.id() === "New Item") {
+                addEntry(activeItem);
+                return;
+            }
             var id = activeItem.id(),
                 match = _.find(inventory(), function(o) {
                     return o.id() === id;
                 });
             match.name(activeItem.name());
             match.rating(activeItem.rating());
-            activeItem.dirty(false);
+            resetForm();
         };
         return {
             inventory: inventory,
